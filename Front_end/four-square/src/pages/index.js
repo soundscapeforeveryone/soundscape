9477633efaf088fdc1fb1c9de689b29e5b4969aa
sMorrison26 <morris10@rpi.edu>
2023-02-22 16:08:59 -0500

    four-square
    Completed four-square app as proposed by mockup

--- Front_end/four-square/src/pages/index.js
+++ Front_end/four-square/src/pages/index.js
@@ -14,13 +14,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
+
+      <div className={styles.items}>
+          <div className={styles.square}>
+            <div id={styles.squareIcon}></div>
+          </div>
+          <div className={styles.circle}>
+            <div id={styles.circleIcon}></div>
+          </div>
+          <div className={styles.triangle}>
+            <div id={styles.triangleIcon}></div>
+          </div>
+          <div className={styles.diamond}>
+            <div id={styles.diamondIcon}></div>
+          </div>
+      </div>
+
+      {/* ================= init app code ================= */}
+      
+      {/* <main className={styles.main}> */}
+        {/* <div className={styles.description}> */}
+          {/* <p>
+            Get  &nbsp;
+            
+          </p> */}
+          {/* <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
@@ -36,9 +54,9 @@ export default function Home() {
                 priority
               />
             </a>
-          </div>
-        </div>
-
+          </div> */}
+        {/* </div> */}
+{/* 
         <div className={styles.center}>
           <Image
             className={styles.logo}
@@ -57,24 +75,24 @@ export default function Home() {
               priority
             />
           </div>
-        </div>
+        </div> */}
 
-        <div className={styles.grid}>
-          <a
+        {/* <div className={styles.grid}> */}
+          {/* <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
+              Changes <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
               Find in-depth information about Next.js features and&nbsp;API.
             </p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -86,9 +104,9 @@ export default function Home() {
             <p className={inter.className}>
               Learn about Next.js in an interactive course with&nbsp;quizzes!
             </p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -100,9 +118,9 @@ export default function Home() {
             <p className={inter.className}>
               Discover and deploy boilerplate example Next.js&nbsp;projects.
             </p>
-          </a>
+          </a> */}
 
-          <a
+          {/* <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
@@ -115,9 +133,9 @@ export default function Home() {
               Instantly deploy your Next.js site to a shareable URL
               with&nbsp;Vercel.
             </p>
-          </a>
-        </div>
-      </main>
+          </a> */}
+        {/* </div> */}
+      {/* </main> */}
     </>
   )
 }
